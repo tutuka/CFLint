4dcdf0965a6ae5085b252fc6f28d13398d6f5dfb
Rohith Rajan <rohithkrajan@gmail.com>
2020-04-19 11:28:38 +0530

    added rules for implements and extends and new component check

--- src/main/java/com/cflint/plugins/core/ComponentUseChecker.java
+++ src/main/java/com/cflint/plugins/core/ComponentUseChecker.java
@@ -1,5 +1,8 @@
 package com.cflint.plugins.core;
 
+import cfml.parsing.cfscript.CFExpression;
+import cfml.parsing.cfscript.script.CFCompDeclStatement;
+import cfml.parsing.cfscript.script.CFCompoundStatement;
 import com.cflint.BugList;
 import com.cflint.plugins.CFLintScannerAdapter;
 import com.cflint.plugins.Context;
@@ -8,9 +11,11 @@ import cfml.parsing.cfscript.script.CFExpressionStatement;
 import cfml.parsing.cfscript.script.CFScriptStatement;
 import ro.fortsoft.pf4j.Extension;
 
+import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.HashMap;
+import com.cflint.tools.CFTool;
 
 @Extension
 public class ComponentUseChecker extends CFLintScannerAdapter {
@@ -33,10 +38,10 @@ public class ComponentUseChecker extends CFLintScannerAdapter {
         patternsMap = new HashMap<UsageTypes, Pattern>();
         patternsMap.put(UsageTypes.CFML_TEMPLATE, Pattern.compile("template=\"(.*)\""));
         patternsMap.put(UsageTypes.CREATEOBJECT, Pattern.compile("createObject\\([\"\\']component[\"\\']\\,[\"\\']([^\\s]+)[\"\\']"));
-        patternsMap.put(UsageTypes.COMPONENT_EXTENDS, Pattern.compile("\\s+extends\\s?=\\s?[\\'\"]([^\\s]+)[\'\"]"));
-        patternsMap.put(UsageTypes.COMPONENT_IMPLEMENTS, Pattern.compile("\\s+implements\\s?=\\s?[\\'\"]([^\\s]+)[\\'\"]"));
+        patternsMap.put(UsageTypes.COMPONENT_EXTENDS, Pattern.compile("[\\'\\\"]([\\w\\.]+)[\\'\\\"]"));
+        //patternsMap.put(UsageTypes.COMPONENT_IMPLEMENTS, Pattern.compile("\\s+implements\\s?=\\s?[\\'\"]([^\\s]+)[\\'\"]"));
         //patternsMap.put(UsageTypes.PROP_INJECT,Pattern.compile("template=\"(.*)\""));
-        patternsMap.put(UsageTypes.NEW_COMPONENT, Pattern.compile("new\\D([a-zA-Z0-9\\.]*)\\(.*"));
+        patternsMap.put(UsageTypes.NEW_COMPONENT, Pattern.compile("new\\s+([\\w\\d.]*)\\(.*"));
         PascalCasePattern = Pattern.compile("^[A-Z][a-z]+(?:[A-Z][a-z]+)*$");
     }
 
@@ -46,18 +51,46 @@ public class ComponentUseChecker extends CFLintScannerAdapter {
             final String code = ((CFExpressionStatement) expression).getExpression().Decompile(0);
             final int lineNo = ((CFExpressionStatement) expression).getLine() + context.startLine() - 1;
             final int offset = ((CFExpressionStatement) expression).getOffset() + context.offset();
-            UsageTypes componentUsage = getComponentUseIfAny(code);
-            if (patternsMap.containsKey(componentUsage)) {
-                Matcher matcher = patternsMap.get(componentUsage).matcher(code);
-                String componentName = matcher.group(1);
-                if (componentName.contains(".")) {
-                    String[] paths = componentName.split(".");
-                    componentName = paths[paths.length - 1];
-                }
-                if (!PascalCasePattern.matcher(componentName).matches()) {
-                    context.addMessage("AVOID_USING_COMPONENT_NAME", null, lineNo, offset);
-                }
+            checkComponentUse(context, code, lineNo, offset);
+        } else if (expression instanceof CFCompDeclStatement) {
+            CFCompDeclStatement compDeclStatement = ((CFCompDeclStatement)expression);
+            Map<String, CFExpression> attributes = CFTool.convertMap(compDeclStatement.getAttributes());
+            CFExpression extendsOrImplements = null;
+            if(attributes.containsKey("extends")) {
+                extendsOrImplements =  attributes.get("extends");
+            } else if(attributes.containsKey("implements"))
+                extendsOrImplements =  attributes.get("implements");
+            if(extendsOrImplements != null){
+                final int lineNo = extendsOrImplements.getLine() + context.startLine() - 1;
+                final int offset = compDeclStatement.getOffset() + context.offset();
+                checkComponentUse(context, extendsOrImplements.Decompile(0), lineNo, offset,UsageTypes.COMPONENT_EXTENDS);
             }
+        }
+
+     }
+
+    private void checkComponentUse(Context context, String code, int lineNo, int offset) {
+        UsageTypes componentUsage = getComponentUseIfAny(code);
+        if (patternsMap.containsKey(componentUsage)) {
+            checkComponentUse(context, code, lineNo, offset, componentUsage);
+        }
+    }
+
+    private void checkComponentUse(Context context, String code, int lineNo, int offset, UsageTypes componentUsage) {
+        Matcher matcher = patternsMap.get(componentUsage).matcher(code);
+        if (matcher.find()) {
+            String componentName = matcher.group(1);
+            verifyComponentUsage(context, lineNo, offset, componentName);
+        }
+    }
+
+    private void verifyComponentUsage(Context context, int lineNo, int offset, String componentName) {
+        if (componentName.contains(".")) {
+            String[] paths = componentName.split("\\.");
+            componentName = paths[paths.length - 1];
+        }
+        if (!PascalCasePattern.matcher(componentName).matches()) {
+            context.addMessage("INVALID_COMPONENT_USAGE", null, lineNo, offset);
         }
     }
 
@@ -71,7 +104,7 @@ public class ComponentUseChecker extends CFLintScannerAdapter {
             return UsageTypes.COMPONENT_EXTENDS;
         } else if (codeLine.contains("implements")) {
             return UsageTypes.COMPONENT_EXTENDS;
-        } else if (codeLine.contains("implements")) {
+        } else if (codeLine.contains("inject")) {
             return UsageTypes.PROP_INJECT;
         } else if (codeLine.contains("template")) {
             return UsageTypes.CFML_TEMPLATE;
--- src/test/java/com/cflint/TestCFBugs_ComponentUse.java
+++ src/test/java/com/cflint/TestCFBugs_ComponentUse.java
@@ -19,17 +19,64 @@ public class TestCFBugs_ComponentUse {
 
     @Before
     public void setUp() throws Exception {
-        final ConfigBuilder configBuilder = new ConfigBuilder().include("AVOID_USING_COMPONENT");
+        final ConfigBuilder configBuilder = new ConfigBuilder().include("INVALID_COMPONENT_USAGE");
         cfBugs = new CFLintAPI(configBuilder.build());
     }
 
     @Test
-    public void testValidNamesTag() throws CFLintScanException {
-        final String tagSrc = "<cfcomponent>\r\n" + "</cfcomponent>";
+    public void testNewComponent() throws CFLintScanException {
+        final String tagSrc = "config = new components.configuration.manager('VoucherEngine', Attributes);";
         CFLintResult lintresult = cfBugs.scan(tagSrc, "NicComponentNm.cfc");
         Collection<List<BugInfo>> result = lintresult.getIssues().values();
-        assertEquals(lintresult.getIssues().values().toString(), 0, result.size());
+        assertEquals(lintresult.getIssues().values().toString(), 1, result.size());
     }
 
+    @Test
+    public void testExtendsComponent() throws CFLintScanException {
+        final String tagSrc = "component extends = 'base' restpath = 'authenticate' rest = true {\n" +
+            " public function init() {\n" +
+            "      return this;\n" +
+            "  }\n" +
+            "}";
+        CFLintResult lintresult = cfBugs.scan(tagSrc, "NicComponentNm.cfc");
+        Collection<List<BugInfo>> result = lintresult.getIssues().values();
+        assertEquals(lintresult.getIssues().values().toString(), 1, result.size());
+    }
+
+    @Test
+    public void testImplementsComponent() throws CFLintScanException {
+        final String tagSrc = "component implements = 'base' restpath = 'authenticate' rest = true {\n" +
+            " public function init() {\n" +
+            "      return this;\n" +
+            "  }\n" +
+            "}";
+        CFLintResult lintresult = cfBugs.scan(tagSrc, "NicComponentNm.cfc");
+        Collection<List<BugInfo>> result = lintresult.getIssues().values();
+        assertEquals(lintresult.getIssues().values().toString(), 1, result.size());
+    }
+
+    @Test
+    public void testImplementsComponentWithFolderPath() throws CFLintScanException {
+        final String tagSrc = "component implements = 'MyProject.interface.handler' {}";
+        CFLintResult lintresult = cfBugs.scan(tagSrc, "NicComponentNm.cfc");
+        Collection<List<BugInfo>> result = lintresult.getIssues().values();
+        assertEquals(lintresult.getIssues().values().toString(), 1, result.size());
+    }
+
+    @Test
+    public void testCreateObject() throws CFLintScanException {
+        final String tagSrc = "server.di = createObject('component', 'ioc.di').init();";
+        CFLintResult lintresult = cfBugs.scan(tagSrc, "NicComponentNm.cfc");
+        Collection<List<BugInfo>> result = lintresult.getIssues().values();
+        assertEquals(lintresult.getIssues().values().toString(), 1, result.size());
+    }
+
+    @Test
+    public void testPropertyInject() throws CFLintScanException {
+        final String tagSrc = "property name = 'reporter' inject = 'components.error.errorfunctions';";
+        CFLintResult lintresult = cfBugs.scan(tagSrc, "NicComponentNm.cfc");
+        Collection<List<BugInfo>> result = lintresult.getIssues().values();
+        assertEquals(lintresult.getIssues().values().toString(), 1, result.size());
+    }
 
 }
